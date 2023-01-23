661baed805de58dfc6ddbe7a2f0f49c9b3e7cd98
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-23 20:07:41 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);*/
+
+function writeCards(n) {
+    let cards = [];
+    for (let i=0; i < n.length; i++) {
+        cards.push(`Thank you, ${n[i]}, for the wonderful surprise gift!`)
+    }
+    return cards;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
